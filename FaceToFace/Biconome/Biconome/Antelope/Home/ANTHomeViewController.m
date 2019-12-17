742a9c30c26380586b6cd4b537692cff79d11672
CheLin <15510373985@163.com>
2019-12-18 01:00:08 +0800

    fix ANT

--- FaceToFace/Biconome/Biconome/Antelope/Home/ANTHomeViewController.m
+++ FaceToFace/Biconome/Biconome/Antelope/Home/ANTHomeViewController.m
@@ -17,18 +17,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self initNavigationTitleViewLabelWithTitle:@"Biconomy" titleColor:[UIColor redColor] IfBelongTabbar:NO];
 
-    // Do any additional setup after loading the view.
+    [self setupUI];
+
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)setupUI
+{
+    self.title = @"清北面对面";
 }
-*/
+
 
 @end
