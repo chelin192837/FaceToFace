9b4d20e9a6ba277133e5f8ae0103189becffef5a
CheLin <15510373985@163.com>
2019-12-29 19:12:32 +0800

    fix 用户名和密码

--- FaceToFace/mall-admin-web-master/src/views/login/index.vue
+++ FaceToFace/mall-admin-web-master/src/views/login/index.vue
@@ -1,26 +1,40 @@
 <template>
+
   <div>
+
     <el-card class="login-form-layout">
+
+
       <el-form autoComplete="on"
                :model="loginForm"
                :rules="loginRules"
                ref="loginForm"
                label-position="left">
+
         <div style="text-align: center">
           <svg-icon icon-class="login-mall" style="width: 56px;height: 56px;color: #409EFF"></svg-icon>
         </div>
+
         <h2 class="login-title color-main">mall-admin-web</h2>
+
+        <!--用户名和密码改变-->
         <el-form-item prop="username">
+
           <el-input name="username"
                     type="text"
                     v-model="loginForm.username"
                     autoComplete="on"
                     placeholder="请输入用户名">
+
           <span slot="prefix">
             <svg-icon icon-class="user" class="color-main"></svg-icon>
           </span>
+
           </el-input>
         </el-form-item>
+
+
+
         <el-form-item prop="password">
           <el-input name="password"
                     :type="pwdType"
@@ -36,14 +50,21 @@
           </span>
           </el-input>
         </el-form-item>
+
         <el-form-item style="margin-bottom: 60px">
           <el-button style="width: 100%" type="primary" :loading="loading" @click.native.prevent="handleLogin">
             登录
           </el-button>
         </el-form-item>
+
       </el-form>
+
     </el-card>
+
+
     <img :src="login_center_bg" class="login-center-layout">
+
+
     <el-dialog
       title="特别赞助"
       :visible.sync="supportDialogVisible"
@@ -72,13 +93,20 @@
 </template>
 
 <script>
+
+  //从别的类中引用方法
   import {isvalidUsername} from '@/utils/validate';
+
+  //
   import {setSupport,getSupport,SupportUrl} from '@/utils/support';
+
+  //导入图片连接,
   import login_center_bg from '@/assets/images/login_center_bg.png'
 
   export default {
     name: 'login',
     data() {
+
       const validateUsername = (rule, value, callback) => {
         if (!isvalidUsername(value)) {
           callback(new Error('请输入正确的用户名'))
@@ -86,6 +114,7 @@
           callback()
         }
       };
+
       const validatePass = (rule, value, callback) => {
         if (value.length < 3) {
           callback(new Error('密码不能小于3位'))
@@ -93,6 +122,7 @@
           callback()
         }
       };
+
       return {
         loginForm: {
           username: 'admin',
@@ -152,6 +182,7 @@
 </script>
 
 <style scoped>
+
   .login-form-layout {
     position: absolute;
     left: 0;
