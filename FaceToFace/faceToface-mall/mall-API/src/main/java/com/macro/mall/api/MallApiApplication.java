aca2a3d26752e4381f20b1b1faa2671dc59a5cfb
CheLin <15510373985@163.com>
2020-08-10 16:29:10 +0800

    fix bug

--- FaceToFace/faceToface-mall/mall-API/src/main/java/com/macro/mall/api/MallApiApplication.java
+++ FaceToFace/faceToface-mall/mall-API/src/main/java/com/macro/mall/api/MallApiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MallApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MallApiApplication.class, args);
+
     }
 }
